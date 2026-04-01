09334f0058009da5998750d09f6b67b4c31ec64c
zenpai0001 <zenpai0001@gmail.com>
2026-04-01 01:02:53 -0400

    Refactor collision interface and add hitbox properties
    ICollidable is now easier to read.

--- Collisions.cs
+++ Collisions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Raylib_cs;
 using System.Collections.Generic;
 using System.Numerics;
@@ -6,24 +6,26 @@ using System.Text;
 
 namespace Raylib_Game_CS_
 {
-    public class Collisions
-    {
-        public Collisions() { }
+
 
         //Checks for collision between two collidable objects. Shared logic for all collision types.
         public interface ICollidable
         {
             public Vector2 Hitbox { get; set; }
-
-            public Vector2 Position { get; set; }
+            
             public float Radius { get; set; }
 
-            public void CollisionCheck(ICollidable collidable)
+            public Rectangle hitbox1 { get; set; }
+           public  Rectangle hitbox2 { get; set; }
+
+        public void CollisionCheck(ICollidable collidable)
             {
-                //Check collision between two rectangles. This is a placeholder for now, but I'll need to workout the logic for this later on.
-                Raylib.CheckCollisionRecs (new Rectangle(Position.X, Position.Y, Hitbox.X, Hitbox.Y), new Rectangle(Position.X, Position.Y, Hitbox.X, Hitbox.Y));
+                Rectangle hitbox1 = new Rectangle(Hitbox.X, Hitbox.Y, Radius * 2, Radius * 2);
+
+                Rectangle hitbox2 = new Rectangle(collidable.Hitbox.X, collidable.Hitbox.Y, collidable.Radius * 2, collidable.Radius * 2);
+
+                Raylib.CheckCollisionRecs(hitbox1, hitbox2);
             }
         }
 
     }
-}
