503b00fcbe4b2aa4a76d5efe20c15c0b46779e2c
zenpai0001 <zenpai0001@gmail.com>
2026-04-01 01:03:53 -0400

    Add jsonFilePath and jsonContent properties
    ISerializable now has fields to work with.

--- Serializer.cs
+++ Serializer.cs
@@ -1,19 +1,21 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
 namespace Raylib_Game_CS_
 {
-    public class Serializer
-    {
         public interface ISerializable
         {
+            string jsonFilePath { get; set; }
+
+            string jsonContent { get; set;  }
+            public void Deserialize(ISerializable serializable)
+            {
+                jsonContent = File.ReadAllText(jsonFilePath);
+            }
             public void Serialize()
             {
-                //Serialization logic goes here.
-                //For now, this is just a placeholder to show where the serialization logic will go.
-                
+
             }
         }
     }
-}
